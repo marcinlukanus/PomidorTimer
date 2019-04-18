6dab7ee586d2f440a6990b60af02821048167207
Marcin <mlukanus@gmail.com>
2019-04-18 17:21:56 -0500

    Initial commit

---
+++ app/src/main/java/com/hotdog/pomidortimer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.hotdog.pomidortimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+//    final Button study = findViewById(R.id.study);
+//    final TextView clock = findViewById(R.id.clock);
+//    final CountDownTimerPausable timer = null;
+//    final long twentyFiveMinutes = 60 * 25;
+//    final long fiveMinutes = 60 * 5;
+//    final long INTERVAL = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //run();
+    }
+
+//    private void run() {
+//        study.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                study.setText("howdy");
+//            }
+//        });
+//    }
+}
